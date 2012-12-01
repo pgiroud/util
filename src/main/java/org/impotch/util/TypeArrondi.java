21a1af516ebae58bf93d1c8f9be4a89d03ffbbf4
Patrick Giroud <patrick.giroud@laposte.net>
2012-12-01 17:55:07 +0100

    Mise à jour indication licence et package

--- src/main/java/org/impotch/util/TypeArrondi.java
+++ src/main/java/org/impotch/util/TypeArrondi.java
@@ -1,19 +1,19 @@
 /**
- * This file is part of CalculImpotCH.
+ * This file is part of impotch/util.
  *
- * CalculImpotCH is free software: you can redistribute it and/or modify
+ * impotch/util is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License.
  *
- * CalculImpotCH is distributed in the hope that it will be useful,
+ * impotch/util is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with CalculImpotCH.  If not, see <http://www.gnu.org/licenses/>.
+ * along with impotch/util.  If not, see <http://www.gnu.org/licenses/>.
  */
-package ch.ge.afc.util;
+package org.impotch.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
