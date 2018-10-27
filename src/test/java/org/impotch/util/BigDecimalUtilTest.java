1d3d88433198abd0c25397ad8a8e59fc928ac9e5
Patrick Giroud <patrick.giroud@laposte.net>
2018-10-27 10:20:37 +0200

    Attention, en java 11, la locale fr_CH utilise la virgule au lieu du point comme séparateur décimal !

--- src/test/java/org/impotch/util/BigDecimalUtilTest.java
+++ src/test/java/org/impotch/util/BigDecimalUtilTest.java
@@ -1,5 +1,20 @@
 /**
  * This file is part of impotch/util.
+ *
+ * impotch/util is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * impotch/util is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with impotch/util.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This file is part of impotch/util.
  * <p>
  * impotch/util is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,8 +46,8 @@ public class BigDecimalUtilTest {
     @Test
     public void formattage() {
         NumberFormat format = NumberFormat.getInstance(SUISSE_ROMANDE);
-        assertThat(BigDecimalUtil.formatRight(new BigDecimal("12.346"), format, 5)).isEqualTo("   12.346");
-        assertThat(BigDecimalUtil.formatRight(new BigDecimal("0.01"), format, 5)).isEqualTo("    0.01");
+        assertThat(BigDecimalUtil.formatRight(new BigDecimal("12.346"), format, 5)).isEqualTo("   12,346");
+        assertThat(BigDecimalUtil.formatRight(new BigDecimal("0.01"), format, 5)).isEqualTo("    0,01");
         assertThat(BigDecimalUtil.formatRight(new BigDecimal("0"), format, 5)).isEqualTo("    0");
         // La suppression des 00 après la virgule est due au format et non à la méthode
         assertThat(BigDecimalUtil.formatRight(new BigDecimal("0.00"), format, 5)).isEqualTo("    0");
